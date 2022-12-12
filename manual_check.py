1cf1cc29a2621e157f42aed21dc897c6677b9ba1
davi-bart <davide30.bartoli@gmail.com>
2022-12-12 11:38:33 +0100

    ask reason if copied

--- manual_check.py
+++ manual_check.py
@@ -4,6 +4,7 @@ import argparse
 import subprocess
 import os
 
+
 def main(args):
     hi = []
     lo = []
@@ -14,18 +15,24 @@ def main(args):
         lo = [l.split() for l in rest[len1:]]
 
     done = {}
-    if os.path.exists(args.output):
-        with open(args.output) as f:
+    if os.path.exists(args.cache):
+        with open(args.cache) as f:
             for line in f.read().splitlines():
-                f1, f2, yn = line.split()
-                done[(f1, f2)] = yn
+                x = line.split()  # file1 file2 outcome
+                outcome = "".join(x[2:])
+                done[(x[0], x[1])] = outcome
 
-    output = open(args.output, "a")
+    cache = open(args.cache, "a")
+    if os.path.exists("output.tsv"):
+        print("output.tsv already exists")
+        exit(0)
+    output = open("output.tsv", "w")
     for name, lst in [("HIGH", hi), ("LOW", lo)]:
         for _, f1, f2 in lst:
             if (f1, f2) in done:
                 continue
-            subprocess.run(["./build/compare", f1, f2, "data/left", "data/right", "data/meta"], check=True, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+            subprocess.run(["./build/compare", f1, f2, "data/left", "data/right", "data/meta"],
+                           check=True, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
             if os.path.exists("data/outcome"):
                 os.remove("data/outcome")
             subprocess.run(["tmux", "-2", "-f", "/dev/null", "start-server", ";", "source-file", "tmux.conf"])
@@ -36,19 +43,21 @@ def main(args):
                 outcome = "q"
             if outcome == "d":
                 continue
-            elif outcome in "yn":
-                output.write("%s %s %s\n" % (f1, f2, outcome))
-                output.flush()
+            elif outcome != "q":
+                cache.write("%s %s %s\n" % (f1, f2, outcome))
+                cache.flush()
                 done[(f1, f2)] = outcome
             else:
                 break
     for (f1, f2), outcome in done.items():
-        if outcome == "y":
-            print(f1, f2)
+        if outcome != "n":
+            output.write("%s\t%s\t%s\n" % (f1, f2, outcome))
+            output.flush()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("results")
-    parser.add_argument("output")
+    parser.add_argument("cache")
     args = parser.parse_args()
     main(args)
--- prompt.py
+++ prompt.py
@@ -6,8 +6,14 @@ print(open("data/meta").read())
 
 prompt = "Are they the same? [Yes|No|Don't know|Quit] (y|n|d|q) "
 r = input(prompt)
-while r not in "yndq":
+while r not in "yndq" or r == "":
     r = input(prompt)
+
+if r == "y":
+    reason = input("Explain why: ")
+    while reason == "":
+        reason = input("Explain why: ")
+    r = reason
 
 with open("data/outcome", "w") as f:
     f.write(r)
